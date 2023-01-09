fdec68e9476c2d1eb4ac2e99b8e52369c99cb17e
davintagas <78451092+davintagas@users.noreply.github.com>
2023-01-09 17:46:29 +0700

    Update diy-part2.sh

--- official/diy-part2.sh
+++ official/diy-part2.sh
@@ -29,6 +29,8 @@ echo "DISTRIB_SOURCECODE='official'" >>package/base-files/files/etc/openwrt_rele
 #
 #
 rm -rf feeds/packages/net/{trojan-go,v2ray-core,v2ray-geodata}
+rm -rf package/libs/libnftnl
+svn co https://github.com/kiddin9/openwrt-packages/trunk/libnftnl package/libs/libnftnl
 
 # Change Firewall
 rm -rf package/network/config/{firewall4,firewall,netifd}
