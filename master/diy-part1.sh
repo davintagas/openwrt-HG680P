534c68f597715b082d405682606b68ed8d0647c2
davintagas <78451092+davintagas@users.noreply.github.com>
2023-01-16 14:36:52 +0700

    Update diy-part1.sh

--- master/diy-part1.sh
+++ master/diy-part1.sh
@@ -12,10 +12,6 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed  -i 's/^dba8a0102e5965cad58a871335002e9c964b6719/;openwrt-22.03/g' feeds.conf.default
-sed  -i 's/^96ec0cd3ccfe954f13fd5a337efdd70374dde03f/;openwrt-22.03/g' feeds.conf.default
-sed  -i 's/^85028704f688a6768d3f10d5d3c10a799a121e0d/;openwrt-22.03/g' feeds.conf.default
-sed  -i 's/^1d2031a5c82816483c51bca15649e2957fbe2bc2/;openwrt-22.03/g' feeds.conf.default
 
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
