f5bfad9e2859a8c8e56159558714730ffe85325a
davintagas <78451092+davintagas@users.noreply.github.com>
2023-11-15 10:14:45 +0700

    Update diy-part1.sh

--- config/openwrt-main/diy-part1.sh
+++ config/openwrt-main/diy-part1.sh
@@ -8,6 +8,10 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i "s|https://git.openwrt.org/feed/packages.git^8e3a1824645f5e73ec44c897ac0755c53fb4a1f8|https://git.openwrt.org/feed/packages.git;openwrt-23.05|g" feeds.conf.default
+sed -i "s|https://git.openwrt.org/project/luci.git^7739e9f5b03b830f51d53c384be4baef95054cb3|https://git.openwrt.org/project/luci.git;openwrt-23.05|g" feeds.conf.default
+sed -i "s|https://git.openwrt.org/feed/routing.git^83ef3784a9092cfd0a900cc28e2ed4e13671d667|https://git.openwrt.org/feed/routing.git;openwrt-23.05|g" feeds.conf.default
+sed -i "s|https://git.openwrt.org/feed/telephony.git^9746ae8f964e18f04b64fbe1956366954ff223f8|https://git.openwrt.org/feed/telephony.git;openwrt-23.05|g" feeds.conf.default
 
 # other
 # rm -rf package/utils/{ucode,fbtest}
