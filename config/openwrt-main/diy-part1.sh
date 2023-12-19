288e2704a232ad2d218030639ef8edab11a68704
davintagas <78451092+davintagas@users.noreply.github.com>
2023-12-19 07:40:34 +0700

    Update diy-part1.sh

--- config/openwrt-main/diy-part1.sh
+++ config/openwrt-main/diy-part1.sh
@@ -8,11 +8,8 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
-sed -i 's/^8e3a1824645f5e73ec44c897ac0755c53fb4a1f8/;openwrt-23.05/g' feeds.conf.default
-sed -i 's/^7739e9f5b03b830f51d53c384be4baef95054cb3/;openwrt-23.05/g' feeds.conf.default
-sed -i 's/^83ef3784a9092cfd0a900cc28e2ed4e13671d667/;openwrt-23.05/g' feeds.conf.default
-sed -i 's/^9746ae8f964e18f04b64fbe1956366954ff223f8/;openwrt-23.05/g' feeds.conf.default
+rm -rf feeds.conf.default
+wget https://raw.githubusercontent.com/openwrt/openwrt/openwrt-23.05/feeds.conf.default
 # other
 # rm -rf package/utils/{ucode,fbtest}
 
